35243530bc9444b01e36ab192b029d45b03ff247
Raphael <raphmens2018@gmail.com>
2024-06-16 23:03:53 +0000

    created all components I will be needing

---
+++ JobSearch/components/homepageComponents/FeaturedJobs.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function FeaturedJobs() {
+  return (
+    <View>
+      <Text>FeaturedJobs</Text>
+    </View>
+  )
+}
